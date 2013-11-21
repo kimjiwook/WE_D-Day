391e262452ee3d467553c50dd68c77d0b9b23445
JWMAC <kimjw@Kimjiwook.local>
2013-11-21 16:41:03 +0900

    Settings….

--- WaterEating_DDay/WE_DDayViewController.h
+++ WaterEating_DDay/WE_DDayViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface WE_DDayViewController : UIViewController
+@interface WE_DDayViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UITableView *ddayTable;
 
 @end
--- WaterEating_DDay/WE_DDayViewController.m
+++ WaterEating_DDay/WE_DDayViewController.m
@@ -13,19 +13,62 @@
 @end
 
 @implementation WE_DDayViewController
+@synthesize ddayTable;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     
-    // git Commit Test
+    [ddayTable setDataSource:self];
+    [ddayTable setDelegate:self];
+    // TableView DataSource, Delegate Setting
+    
+    self.navigationItem.title = @"D-Day";
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    
+    NSArray *viewsToRemove = [cell.contentView subviews];
+    for (UIView *v in viewsToRemove)
+    {
+        if (v.tag == 1000) {
+            [v removeFromSuperview];
+        }
+        
+    }
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(5, 10, 100, 30)];
+    [title setText:[NSString stringWithFormat:@"제목란 : %d",indexPath.row+1]];
+    [title setBackgroundColor:[UIColor clearColor]];
+    [title setFont:[UIFont systemFontOfSize:14.0f]];
+    [title setTag:1000];
+    
+    [cell.contentView addSubview:title];
+    
+    return cell;
 }
 
 @end

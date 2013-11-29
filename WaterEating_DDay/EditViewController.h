6eaea4f7909fac6e963ef5b1f6cf2eb05591b882
JWMAC <kimjw@Kimjiwook.local>
2013-11-29 10:09:48 +0900

    method add

--- WaterEating_DDay/EditViewController.h
+++ WaterEating_DDay/EditViewController.h
@@ -13,12 +13,13 @@
 
 @property (nonatomic, strong) IBOutlet UITableView *addTable;
 
-@property (nonatomic, strong) IBOutlet UIDatePicker *datePicker;
+@property (nonatomic, strong) IBOutlet UIDatePicker *datePicker; // 오늘 날짜 (date)
 @property (nonatomic, strong) IBOutlet UILabel *dayLabel;
-@property (strong, nonatomic) IBOutlet UISwitch *oneDayCheckSwitch;
-@property (strong, nonatomic) IBOutlet UITextField *subJectTextField;
+@property (strong, nonatomic) IBOutlet UISwitch *oneDayCheckSwitch; // 시작일 +1일 (Bool)
+@property (strong, nonatomic) IBOutlet UITextField *subJectTextField; // 제목 (String)
 
 - (IBAction)dateChanged:(id)sender;
 - (IBAction)onDayCheckAction:(id)sender;
+- (IBAction)daySave:(id)sender;
 
 @end

e9971529648eecbb9be7f20432106e82010ed43c
JWMAC <kimjw@Kimjiwook.local>
2013-11-29 10:09:21 +0900

    App Delegate update

--- WaterEating_DDay/WE_DDayAppDelegate.m
+++ WaterEating_DDay/WE_DDayAppDelegate.m
@@ -12,7 +12,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    [MagicalRecord setupCoreDataStack];
+    // 시작에 앞서 MagicalRecord setup 시켜준다.
+    
     return YES;
 }
 							

6660be0aa452217baac0e9987dba0167fc9ec136
JWMAC <kimjw@Kimjiwook.local>
2013-12-02 09:55:47 +0900

    DetailView add..

---
+++ DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  WaterEating_DDay
+//
+//  Created by JWMAC on 2013. 12. 2..
+//  Copyright (c) 2013년 KimJiWook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end

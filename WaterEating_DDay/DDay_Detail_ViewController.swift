7383c84a195b4b4b6733a22ac64f5fd771f1af87
kimjiwook <kimjiwook@kimjiwookui-MacBook-Pro.local>
2015-10-04 23:32:38 +0900

    iOS 9 이후 글씨 깨짐 현상으로 인한 변경 완료본.

---
+++ WaterEating_DDay/DDay_Detail_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DDay_Detail_ViewController.swift
+//  WaterEating_DDay
+//
+//  Created by kimjiwook on 2015. 9. 30..
+//  Copyright © 2015년 KimJiWook. All rights reserved.
+//
+
+import UIKit
+
+class DDay_Detail_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WaterEating_DDay/Detail_TopInfo_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Detail_TopInfo_TableViewCell.swift
+//  WaterEating_DDay
+//
+//  Created by kimjiwook on 2015. 10. 3..
+//  Copyright © 2015년 KimJiWook. All rights reserved.
+//
+
+import UIKit
+
+class Detail_TopInfo_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
